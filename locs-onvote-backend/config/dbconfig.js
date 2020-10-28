1cc2dcc6ba965c16c27c8de6356d9ac5fe8caf2b
root <root@ip-172-31-56-221.ap-northeast-2.compute.internal>
2020-10-28 17:01:08 +0900

    디비정보 아마존으로 변경

--- locs-onvote-backend/config/dbconfig.js
+++ locs-onvote-backend/config/dbconfig.js
@@ -1,7 +1,7 @@
 module.exports = {
-    host: '211.236.48.216',
+    host: '127.0.0.1',
     port: 3306,
-    user: 'root',
+    user: 'locs',
     password: 'locslab',
     connectionLimit: 10,
     database: 'onvote',
